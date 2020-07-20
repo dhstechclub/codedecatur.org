25f7aa88f4091b5a43d445794c93b20b4367cacb
xHayden <93haydcarp@csdecatur.net>
2020-07-19 23:52:24 -0400

    Sitemap and fixed url

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,9 @@ module.exports = {
   siteMetadata: {
     title: `Code Decatur`,
     description: `Code Decatur is a Coding Organization created to help students at all skill levels develop the necessary skills to become and experienced developer. We are also a Coding Club at Decatur High School in Decatur, Georgia 30030. We offer coding tutorials, and feature the projects of students in our organization. Our goal is to create a good sustainable Computer Science option for Decatur students. We offer tutorials on Python, Javascript, C#, Java, Assembly, and more! We are Student-Driven and output a steady stream of content, both for the members of our club to use and for the public to enjoy. We also host competitions to encourage an innovative environment. We meet every Wednesday to work on individual projects alongside group tutorials of all skills levels. `,
-    url: "https://codecatur.com",
+    url: "https://codedecatur.org",
     author: `Hayden Carpenter`,
+    siteUrl: `https://codedecatur.org`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -73,6 +74,8 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-sitemap`,
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
