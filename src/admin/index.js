df6475b5adb7a34f622d0c177fd1b8f6b355f730
xHayden <93haydcarp@csdecatur.net>
2020-08-02 15:48:57 -0400

    fix?

--- src/admin/index.js
+++ src/admin/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const Admin = (props) => {
     return <div>
-        Admin
         <script src="https://identity-js.netlify.com/v1/netlify-identity-widget.js"></script>
         <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-        
     </div>
 }
 
