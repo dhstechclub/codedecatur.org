a3db5ffe0a7b3baf6edeb29b34ffa1b8093ee4fc
xHayden <93haydcarp@csdecatur.net>
2020-08-02 15:09:59 -0400

    Tests with Netlify

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Admin = (props) => {
-    return <div>Admin</div>
+    return <div>
+        Admin
+        <script src="https://identity-js.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+        
+    </div>
 }
 
 export default Admin;
