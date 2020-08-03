0517f8f2e9a9fa5e31f33303ff3b4e2cc94318f0
xHayden <haydencarp@outlook.com>
2020-08-03 17:42:23 -0400

    its even worse when its wrong

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -33,9 +33,10 @@ const LanguageCheckbox = (props) => {
     let langUpper = props.language.charAt(0).toUpperCase() + props.language.slice(1)
     if(langUpper == "Csharp"){
         langUpper = "C#";
-    }
-    else if(langUpper = "Cplusplus"){
+    } else if(langUpper = "Cplusplus"){
         langUpper = "C++";
+    } else {
+        continue;
     }
     return (
         <button style={{'marginRight': "20px", 'backgroundColor': 'rgba(0, 255, 0, 0.2)'}} onClick={() => props.setlangs(toggleLanguage(props.langs, props.language, checkboxName))} id={checkboxName}><p htmlFor={checkboxName} id={labelName} style={{'marginBottom': 0}}>{langUpper}</p></button>
