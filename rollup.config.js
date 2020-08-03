b6d0bff3332f16b6812f542e404a1a42a7b8cce3
xHayden <93haydcarp@csdecatur.net>
2020-08-03 01:42:32 -0400

    Testing themes

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const builtins = require('rollup-plugin-node-builtins');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const json = require('rollup-plugin-json');
+const babel = require('rollup-plugin-babel');
+
+export default {
+  input: 'src/admin/tutorial-preview.js',
+  output: {
+    file: 'dist/admin/tutorial-preview.js',
+    format: 'esm',
+  },
+  plugins: [
+    nodeResolve({browser:true}),
+    commonjs({ignore: ["conditional-runtime-dependency"]}),
+    builtins(),
+    json(),
+    babel({
+      "plugins": [
+        ["prismjs", {
+          "languages": ["javascript", "css", "markup", "python", "clike"]
+        }]
+      ]
+    })
+  ]
+};
---
+++ src/admin/tutorial-preview.js
@@ -0,0 +1,44 @@
+// Netlify CMS exposes two React method "createClass" and "h"
+import htm from 'htm';
+import markdownIt from "markdown-it";
+import markdownItKatex from "@iktakahiro/markdown-it-katex";
+import { CMS, h, createClass } from 'netlify-cms-app';
+import Prism from "prismjs";
+const html = htm.bind(h); 
+ 
+let options = {
+    html: true,
+    typographer: true,
+    linkify: true,
+    highlight: function (str, lang) {
+      var languageString = "language-" + lang;
+      if (Prism.languages[lang]) {
+        return '<pre class="language-' + lang + '"><code class="language-' + lang + '">' + Prism.highlight(str, Prism.languages[lang], lang) + '</code></pre>';
+      } else {
+        return '<pre class="language-' + lang + '"><code class="language-' + lang + '">' + Prism.util.encode(str) + '</code></pre>';
+      }
+    }
+  };
+var customMarkdownIt = new markdownIt(options);
+
+var Tutorial = createClass({
+    render() {
+      const entry = this.props.entry;
+      const title = entry.getIn(["data", "title"], null);
+      const body = entry.getIn(["data", "body"], null);
+      const bodyRendered = customMarkdownIt.render(body || '');
+  
+      return html`
+      <body>
+        <main>
+          <article>
+            <h1>${title}</h1>
+            <div dangerouslySetInnerHTML=${{__html: bodyRendered}}></div>
+          </article>
+        </main>
+      </body>
+      `;
+    }
+  });
+  
+CMS.registerPreviewTemplate('tutorial', Tutorial);
